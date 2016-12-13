51141d291fc9ca5e7b54ba3a281025f8562aa7a1
Yeni <yenicapote08@gmail.com>
2016-12-13 15:23:05 -0800

    Added tests for empty and filled functions.

--- spec/board.spec.js
+++ spec/board.spec.js
@@ -1,6 +1,4 @@
-import Game from 'tic-tac-toe';
-import Player from 'tic-tac-toe';
-import Board from 'tic-tac-toe';
+import Board from 'board';
 
 describe('Board', function() {
   var testBoard = new Board();
@@ -8,5 +6,22 @@ describe('Board', function() {
     it('should return true if all spots in board are empty', function() {
         expect(testBoard.empty()).toEqual(true);
     });
+
+    it('should return false if one spot is filled', function() {
+      testBoard.board[0][1] = "X";
+      expect(testBoard.empty()).toEqual(false);
+    })
   });
+
+  describe('filled', function() {
+    it('should return true if that spot in board is filled', function() {
+        expect(testBoard.filled(0,0)).toEqual(false);
+    });
+
+    it('should return false if one spot is filled', function() {
+      testBoard.board[0][1] = "X";
+      expect(testBoard.filled(0,1)).toEqual(true);
+    })
+  });
+
 });
--- src/board.js
+++ src/board.js
@@ -1,4 +1,29 @@
-var Board = function() {};
+var Board = function() {
+  this.board = new Array(3);
+  // write a for loop
+  this.board[0] = new Array(3);
+  this.board[1] = new Array(3);
+  this.board[2] = new Array(3);
+};
 
+Board.prototype.empty = function () {
+  var isEmpty = true;
+  for (var i = 0; i < this.board.length; i++) {
+    for (var j= 0; j < this.board[i].length; j++) {
+      if (this.board[i][j] !== undefined) {
+        isEmpty = false;
+      }
+    }
+    return isEmpty;
+  };
+};
+
+Board.prototype.filled = function (i, j) {
+  var isFilled = false;
+  if (this.board[i][j] !== undefined) {
+    isFilled = true;
+  }
+  return isFilled;
+};
 
 export default Board;
