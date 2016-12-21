03e31d840d69798f657592bb3ed9097af8b7fae3
Yeni <yenicapote08@gmail.com>
2016-12-21 11:36:45 -0800

    cleaned up specs and removed pieces of play function that we no longer need.

--- spec/game.spec.js
+++ spec/game.spec.js
@@ -15,7 +15,6 @@ describe('Game', function() {
      expect(testGame.player1).toEqual("x");
      expect(testGame.player2).toEqual("o");
      expect(testGame.counter).toEqual(1);
-     expect(testGame.mark).toEqual("x");
    });
  });
 
@@ -24,12 +23,7 @@ describe('Game', function() {
     it('should assign the right mark and player', function() {
       testGame.play(0, 1);
       expect(testGame.player).toBe("x");
-      // console.log("this is mark " + testGame.mark);
-      // expect(testGame.mark).toEqual("x");
-    });
-
-    it('should fill the spot with the designated mark, if empty', function() {
-      expect(testGame.play(0, 1)).toBe("x picked spot [0][1]");
+      expect(testGame.mark).toEqual("x");
     });
 
     it('should increment counter by one', function() {
@@ -43,7 +37,6 @@ describe('Game', function() {
       var check = testGame.check;
       testGame.play(1, 1);
       testGame.play(0, 0);
-      expect(testGame.play(0, 0)).toBe("Spot taken! x try again.");
       expect(testGame.counter).toEqual(3);
       expect(testGame.board.checkBoard()).toEqual([ [ 'o', undefined, undefined ], [ undefined, 'x', undefined ], [ undefined, undefined, undefined ] ]);
     });
@@ -55,7 +48,6 @@ describe('Game', function() {
         testGame.play(1, 1);
         testGame.play(2, 1);
         testGame.play(1, 2);
-        // expect(testGame.play(1, 2)).toEqual("x has won!");
         expect(testGame.won()).toEqual(true);
       });
 
@@ -66,7 +58,6 @@ describe('Game', function() {
         testGame.play(0, 1);
         testGame.play(2, 2);
         testGame.play(0, 2);
-        // expect(testGame.won()).toEqual("o has won!");
         expect(testGame.won()).toEqual(true);
       });
 
@@ -76,7 +67,6 @@ describe('Game', function() {
         testGame.play(2, 1);
         testGame.play(1, 2);
         testGame.play(2, 2);
-        // expect(testGame.won()).toEqual("x has won!");
         expect(testGame.won()).toEqual(true);
       });
 
@@ -87,9 +77,7 @@ describe('Game', function() {
         testGame.play(1, 0);
         testGame.play(2, 1);
         testGame.play(2, 0);
-        // expect(testGame.won()).toEqual("o has won!");
         expect(testGame.won()).toEqual(true);
-        // expect(testGame.board.checkBoard()).toEqual([ [ undefined, undefined, undefined ], [ undefined, undefined, undefined ], [ undefined, undefined, undefined ] ]);
       });
 
       it('should check for a win if either player has won verticaly middle', function() {
@@ -99,9 +87,7 @@ describe('Game', function() {
         testGame.play(1, 1);
         testGame.play(1, 2);
         testGame.play(2, 1);
-        // expect(testGame.won()).toEqual("o has won!");
         expect(testGame.won()).toEqual(true);
-        // expect(testGame.board.checkBoard()).toEqual([ [ undefined, undefined, undefined ], [ undefined, undefined, undefined ], [ undefined, undefined, undefined ] ]);
       });
 
       it('should check for a win if either player has won verticaly right', function() {
@@ -110,7 +96,6 @@ describe('Game', function() {
         testGame.play(1, 2);
         testGame.play(2, 1);
         testGame.play(2, 2);
-        // expect(testGame.won()).toEqual("x has won!");
         expect(testGame.won()).toEqual(true);
       });
 
@@ -121,8 +106,6 @@ describe('Game', function() {
         testGame.play(1, 1);
         testGame.play(1, 2);
         testGame.play(2, 2);
-
-        // expect(testGame.won()).toEqual("o has won!");
         expect(testGame.won()).toEqual(true);
       });
       it('should check for a win if either player has won diagonaly right to left', function() {
@@ -131,8 +114,6 @@ describe('Game', function() {
         testGame.play(1, 1);
         testGame.play(1, 0);
         testGame.play(2, 0);
-        console.log(testGame.board.board);
-        // expect(testGame.won()).toEqual("x has won!");
         expect(testGame.won()).toEqual(true);
       });
 
@@ -146,10 +127,7 @@ describe('Game', function() {
         testGame.play(0, 1);
         testGame.play(1, 0);
         testGame.play(1, 2);
-
-        // expect(testGame.won()).toEqual("It's a tie");
         expect(testGame.won()).toEqual(false);
-        // expect(testGame.board.checkBoard()).toEqual([ [ undefined, undefined, undefined ], [ undefined, undefined, undefined ], [ undefined, undefined, undefined ] ]);
       });
     });
   });
--- src/app/models/game.js
+++ src/app/models/game.js
@@ -9,12 +9,9 @@ const Game = Backbone.Model.extend({
     this.player1 = "x";
     this.player2 ="o";
     this.counter = 1;
-    this.mark = "x";
   },
 
   play: function(row, column) {
-    // // this.mark = "";
-
 
     if(this.counter % 2 == 1) {
       this.player = this.player1;
@@ -26,60 +23,60 @@ const Game = Backbone.Model.extend({
     }
 
     var reply;
+    var winning = this.won();
     var isFilled  =  this.board.filled(row,column);
-    if(isFilled === false){
+    if(isFilled === false && winning === false){
       this.board.fill(row, column, this.mark);
-      reply = this.player + " picked spot " + "[" + row + "]" + "[" + column + "]";
       this.counter += 1;
       this.trigger('change'); // to trigger render for each play
     }
-    else {
-      reply = "Spot taken! " + this.player + " try again.";
-    }
 
-    var winning = this.won();
+
 
     if (winning === true) {
-      if (this.wonMark === 'x') {
-        reply = this.player1 + " has won!";
+      if (reply === undefined) {
+        if (this.wonMark === 'x') {
+          reply = this.player1 + " has won!";
+        }
+        else if (this.wonMark === 'o') {
+          reply = this.player2 + " has won!";
+        }
       }
-      else if (this.wonMark === 'o') {
-        reply = this.player2 + " has won!";
-      }
-    }
-    else if (this.counter === 9) {
+      else if (this.counter === 9) {
 
-      reply = "It's a tie!";
+        reply = "It's a tie!";
+      }
     }
-    return reply;
+    console.log(reply);
+    // return reply;
   },
 
   won: function() {
 
     this.hasWon = false;
-    this.mark = "";
+    this.wonMark = "x";
 
     //horizontal
     for (var index = 0; index < this.board.board.length; index++) {
       if (this.board.board[index][0] === this.board.board[index][1] && this.board.board[index][1] === this.board.board[index][2] && this.board.board[index][0] !== undefined) {
         this.hasWon = true;
-        this.mark = this.board.board[index][0];
+        this.wonMark = this.board.board[index][0];
       }
       //vertical win
       else if (this.board.board[0][index] === this.board.board[1][index] && this.board.board[1][index] === this.board.board[2][index] && this.board.board[0][index] !== undefined) {
         this.hasWon = true;
-        this.mark = this.board.board[0][index];
+        this.wonMark = this.board.board[0][index];
       }
 
     }
     //diagonal
     if (this.board.board[0][0] === this.board.board[1][1] && this.board.board[1][1] === this.board.board[2][2] && this.board.board[1][1] !== undefined) {
       this.hasWon = true;
-      this.mark = this.board.board[1][1];
+      this.wonMark = this.board.board[1][1];
     }
     else if (this.board.board[0][2] === this.board.board[1][1] && this.board.board[1][1] === this.board.board[2][0] && this.board.board[1][1] !== undefined) {
       this.hasWon = true;
-      this.mark = this.board.board[1][1];
+      this.wonMark = this.board.board[1][1];
     }
     if (this.hasWon === true) {
       // this.board.clearBoard();
