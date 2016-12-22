fb11cb74480f1e8e32b35af04d560d3491255c77
Beylul <bkbreab8@gmail.com>
2016-12-22 11:41:39 -0800

    Added new collection folder and list_game

---
+++ src/app/collections/list_game.js
@@ -0,0 +1,9 @@
+import Backbone from 'backbone';
+import Game from 'app/models/game';
+
+var GameList = Backbone.Collection.extend({
+     model: Game,
+     url: 'http://localhost:3000/api/v1/games',
+});
+
+export default GameList;
